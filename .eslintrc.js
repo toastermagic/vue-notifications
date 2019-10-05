b68a2f2a031f26318161360bab82fc43ec1116da
paul <toastermagic@gmail.com>
2019-10-05 18:02:58 +0100

    change rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,20 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript'
+  "extends": [
+    "plugin:vue/essential",
+    "@vue/standard",
+    "@vue/typescript"
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "comma-dangle": ["warn", "only-multiline"],
+    "quotes": ["error", "double"],
+    "space-before-function-paren": ["warn", "never"],
+    "semi": ["warn", "always"],
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: "@typescript-eslint/parser"
   }
 }
